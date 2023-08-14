9c4f2d8292c4327370f4f076ecae1bdf25de5b4e
Алексей Осипычев <alexey.osipychev@yandex.ru>
2023-08-14 19:58:01 +0200

    move messages into separate file

---
+++ common/messages.py
@@ -0,0 +1,36 @@
+REGISTER_URL_INPUT = 'Введите стандартный url для регистрации: '
+PSEUDONYM_INPUT = 'Введите псевдоним домашней страницы url: '
+SHORT_URL_INPUT = 'Введите сокращенный url: '
+
+SHORT_URL_OUTPUT = 'Короткий интернет адрес: {}'
+PSEUDONYM_OUTPUT = 'Псевдоним домашней страницы: {}'
+URL_OUTPUT = 'Стандартный интернет-адрес: {}'
+RESPONSE_CODE_OUTPUT = 'Код ответа страницы: {}'
+
+PSEUDONYM_NOT_FOUND = 'Страница с таким псевдонимом не найдена'
+SHORT_URL_NOT_FOUND = 'Страница не найдена'
+
+INVALID_URL_ERROR = ('Неверный URL. URL должен начинаться на https:// '
+                     'и должен содержать домены нулевого и первого уровней\n'
+                     '(прим. https://www.aviasales.org)')
+DOMAIN_TOO_SHORT_ERROR = ('Неверный URL. Домен должен быть длиннее 1 символа\n'
+                          '(прим. https://www.aviasales.org)')
+
+MENU_LABEL = 'Введите'
+MENU_CHOICE_INPUT = 'Ваш выбор: '
+MENU_REDUCE_URL = 'регистрация короткого интернет-адреса по стандартному'
+MENU_GET_HOMEPAGE_BY_PSEUDONYM = 'получение и проверка домашней страницы интернет-адреса по псевдониму'
+MENU_GET_FULL_URL_BY_SHORT = 'получение и проверка стандартного интернет-адреса по короткому'
+MENU_GET_ALL_DATA = 'получение всех пар адресов'
+MENU_STOP_APP = 'завершение программы'
+MENU_CHOICE_ERROR = 'Неверное значение. Укажите число от 1 до 5'
+
+PSEUDONYM_LIST_LABEL = 'Псевдонимы:'
+SHORT_URL_LIST_LABEL = 'Короткие интернет-адреса:'
+STOP_APP = 'Завершение работы программы'
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from common import exceptions
+from common import exceptions, messages
 from common.settings import DATA_FILE_PATH
 from common.utils.regex import shorten_url, get_domain, get_homepage
 from common.utils.storage import load_data_from_json, Storage, save_data_to_json
@@ -16,84 +16,84 @@ def main():
 
 def choice_options(storage: Storage):
     selection_options = {
-        1: ('регистрация короткого интернет-адреса по стандартному', reduce_url, True),
-        2: ('получение и проверка домашней страницы интернет-адреса по псевдониму', get_homepage_by_pseudonym, True),
-        3: ('получение и проверка стандартного интернет-адреса по короткому', get_full_url_by_short, True),
-        4: ('получение всех пар адресов', get_all_data, True),
-        5: ('завершение программы', stop_app, False),
+        1: (messages.MENU_REDUCE_URL, reduce_url, True),
+        2: (messages.MENU_GET_HOMEPAGE_BY_PSEUDONYM, get_homepage_by_pseudonym, True),
+        3: (messages.MENU_GET_FULL_URL_BY_SHORT, get_full_url_by_short, True),
+        4: (messages.MENU_GET_ALL_DATA, get_all_data, True),
+        5: (messages.MENU_STOP_APP, stop_app, False),
     }
 
-    print('\nВведите')
+    print()
+    print(messages.MENU_LABEL)
     for key, value in selection_options.items():
         print(f'{key} - {value[0]}')
 
-    choice = int(input('\nВаш выбор: '))
+    choice = int(input('\n' + messages.MENU_CHOICE_INPUT))
     if choice in selection_options:
         selection_options[choice][1](storage)
         return selection_options[choice][2]
 
     else:
-        print('Введите значение от 1 до 5')
+        print(messages.MENU_CHOICE_ERROR)
         return True
 
 
 def reduce_url(storage: Storage):
-    url = input('Введите стандартный url для регистрации: ')
+    url = input(messages.REGISTER_URL_INPUT)
     try:
         pseudonym = get_domain(url)
         home_page = get_homepage(url)
     except exceptions.InvalidUrl as e:
-        print('Неверный URL. URL должен начинаться на https:// и должен содержать домены нулевого и первого уровней\n'
-              '(прим. https://www.aviasales.org)')
+        print(messages.INVALID_URL_ERROR)
         return
     try:
         short_url = shorten_url(url)
     except exceptions.UrlDomainTooShort as e:
-        print('Неверный URL. Домен должен быть длиннее 1 символа\n'
-              '(прим. https://www.aviasales.org)')
+        print(messages.DOMAIN_TOO_SHORT_ERROR)
         return
     storage.add_pseudonym(pseudonym, home_page)
     storage.add_short_url(short_url, url)
-    print(f'Короткий интернет адрес: {short_url}')
-    print(f'Псевдоним домашней страницы: {pseudonym}')
-    print(f'Стандартный интернет-адрес: {url}')
+    print(messages.SHORT_URL_OUTPUT.format(short_url))
+    print(messages.PSEUDONYM_OUTPUT.format(pseudonym))
+    print(messages.URL_OUTPUT.format(url))
 
 
 def get_homepage_by_pseudonym(storage: Storage):
-    pseudonym = input('Введите псевдоним домашней страницы url: ')
+    pseudonym = input(messages.PSEUDONYM_INPUT)
     if storage.has_pseudonym(pseudonym):
         home_page = storage.get_homepage_url(pseudonym)
         response_code = get_response_code_url(home_page)
-        print(f'Стандартный интернет-адрес: {home_page}')
-        print(f'Псевдоним домашней страницы интернет-адрес: {pseudonym}')
-        print(f'Код ответа страницы: {response_code}')
+        print(messages.URL_OUTPUT.format(home_page))
+        print(messages.PSEUDONYM_OUTPUT.format(pseudonym))
+        print(messages.RESPONSE_CODE_OUTPUT.format(response_code))
     else:
-        print('Страница с таким псевдонимом не найдена')
+        print(messages.PSEUDONYM_NOT_FOUND)
 
 
 def get_full_url_by_short(storage: Storage):
-    short_url = input('Введите сокращенный url: ')
+    short_url = input(messages.SHORT_URL_INPUT)
     if storage.has_short(short_url):
         full_url = storage.get_full_url(short_url)
         response_code = get_response_code_url(full_url)
-        print(f'Стандартный интернет-адрес: {full_url}')
-        print(f'Короткий интернет-адрес: {short_url}')
-        print(f'Код ответа страницы: {response_code}')
+        print(messages.URL_OUTPUT.format(full_url))
+        print(messages.SHORT_URL_OUTPUT.format(short_url))
+        print(messages.RESPONSE_CODE_OUTPUT.format(response_code))
     else:
-        print('Страница не найдена')
+        print(messages.SHORT_URL_NOT_FOUND)
 
 
 def get_all_data(storage: Storage):
-    print('Псевдонимы:')
+    print(messages.PSEUDONYM_LIST_LABEL)
     for item in storage.pseudonyms.items():
         print(item)
-    print('\nКороткие интернет-адреса:')
+    print()
+    print(messages.SHORT_URL_LIST_LABEL)
     for item in storage.shorts.items():
         print(item)
 
 
 def stop_app(storage: Storage):
-    print('Завершение работы программы')
+    print(messages.STOP_APP)
     save_data_to_json(storage, DATA_FILE_PATH)
 
 
